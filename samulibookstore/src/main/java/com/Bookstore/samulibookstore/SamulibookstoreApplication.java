b0278220561146eb2d3029cb3fac43ba4ddf22f8
samul <samul@DESKTOP-9F66L82.home>
2022-09-12 14:13:01 +0300

    initial commit

---
+++ samulibookstore/src/main/java/com/Bookstore/samulibookstore/SamulibookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.Bookstore.samulibookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamulibookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamulibookstoreApplication.class, args);
+	}
+
+}
---
+++ samulibookstore/src/test/java/com/Bookstore/samulibookstore/SamulibookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bookstore.samulibookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamulibookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
