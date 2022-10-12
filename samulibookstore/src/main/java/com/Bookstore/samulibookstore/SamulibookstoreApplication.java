6facf0ace86876041e80380ac7ce49014a0e626c
samul <samul@DESKTOP-9F66L82.home>
2022-10-12 19:26:22 +0300

    sdfsdfdsfsahdf sf hj dftyjd tjd

--- samulibookstore/src/main/java/com/Bookstore/samulibookstore/SamulibookstoreApplication.java
+++ samulibookstore/src/main/java/com/Bookstore/samulibookstore/SamulibookstoreApplication.java
@@ -23,17 +23,17 @@ public class SamulibookstoreApplication  {
 	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		
 		return(args)->{
-			urepository.save(new User("user", "$2y$10$iyEr.tfpe9q2NrY0B9ayAeVsd4ljb5VHP8y/ioV/rFXrz2fv624yG", "USER"));
-			urepository.save(new User("admin", "$2y$10$6P79CpftG0MpMstXlAgieuIZA3fRCD89iXHc8L6zTAcTLPyxjDAse", "ADMIN"));
+			//urepository.save(new User("user", "$2y$10$iyEr.tfpe9q2NrY0B9ayAeVsd4ljb5VHP8y/ioV/rFXrz2fv624yG", "USER"));
+			//urepository.save(new User("admin", "$2y$10$6P79CpftG0MpMstXlAgieuIZA3fRCD89iXHc8L6zTAcTLPyxjDAse", "ADMIN"));
 			
-			crepository.save(new Category("-"));
-			crepository.save(new Category("ismo kirjat"));
-			crepository.save(new Category("tomaatti"));
+			//crepository.save(new Category("-"));
+			//crepository.save(new Category("ismo kirjat"));
+			//crepository.save(new Category("tomaatti"));
 			
-			brepository.save(new Book("Seppo Taalasmaa", "juupeli", 2010, "197434-2342", 345, crepository.findByName("-").get(0)));
-			brepository.save(new Book("Ismo Laitela", "jogggo", 2017, "197434-2342", 56, crepository.findByName("-").get(0)));
-			brepository.save(new Book("Suppo Laitela", "g", 2022, "197434-2342", 46, crepository.findByName("-").get(0)));
-			brepository.save(new Book("Osteri Ossi", "jofhhhfo", 1989, "197434-2342", 868, crepository.findByName("-").get(0)));
+			//brepository.save(new Book("Seppo Taalasmaa", "juupeli", 2010, "197434-2342", 345, crepository.findByName("-").get(0)));
+			//brepository.save(new Book("Ismo Laitela", "jogggo", 2017, "197434-2342", 56, crepository.findByName("-").get(0)));
+			//brepository.save(new Book("Suppo Laitela", "g", 2022, "197434-2342", 46, crepository.findByName("-").get(0)));
+			//brepository.save(new Book("Osteri Ossi", "jofhhhfo", 1989, "197434-2342", 868, crepository.findByName("-").get(0)));
 		};
 		
 	}
